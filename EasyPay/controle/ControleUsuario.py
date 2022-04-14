46cf9ec75010b5c0615f1af47e6b2bd62f8f2cf7
Matheus <rch00.matheus@gmail.com>
2022-04-13 23:41:42 -0300

    controle usuário com banco de dados

--- EasyPay/controle/ControleUsuario.py
+++ EasyPay/controle/ControleUsuario.py
@@ -1,4 +1,5 @@
 from utils.ValidaFormulario import ValidaFormulario
+from controle.ControleBanco import ConnEasyPayDB
 from entidades.Usuario import Usuario
 
 
@@ -10,7 +11,12 @@ class ControleUsuario:
     def adiciona_usuario(self, login, senha):
         if ValidaFormulario.login(login) and ValidaFormulario.senha(senha):
             novo_usuario = Usuario(login, senha)
+            adiciona_usuario_sql = '''
+                INSERT INTO USUARIOS (LOGIN, SENHA)
+                VALUES (?, ?);
+            '''
             self.usuarios.append(novo_usuario)
+            ConnEasyPayDB.execute_sql(adiciona_usuario_sql, novo_usuario.collect())
 
 
 
